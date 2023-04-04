94ba6901aa27a12ad18c349a274416a5db30df16
komal jha <kjha4951@gmail.com>
2023-04-04 09:38:37 +0530

    animation

---
+++ two.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.bgcolor("light blue")
+turtle.pensize(4.5)
+turtle.speed(0.5)
+# turtle.bgpic("mod.gif")
+color=["orange","white","green"]
+for x in range(19):
+    for i in color:
+        turtle.color(i)
+        turtle.circle(150)
+        turtle.left(10)
